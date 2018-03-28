fa0b5a44e38ff4d44dde8b980c6056d7bcba3dc5
Jikkai Xiao <sonne@asaki.me>
2018-03-28 18:44:18 +0800

    update an example

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 require('dotenv').config()
 const Telegraf = require('telegraf')
+const Axios = require('axios')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.command('/oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('/modern', ({ reply }) => reply('Yo'))
-bot.command('/hipster', Telegraf.reply('λ'))
+const axios = Axios.create({
+  baseURL: 'https://api.vc.bilibili.com/link_draw/v2'
+})
+
+bot.command('/pics', async ctx => {
+  console.log(`Fetching Start`)
+  const message = ctx.message.text.trim().toLowerCase()
+  let num = 0
+  try {
+    num = message.match(/^\/pics ([0-9]+)$/)[1]
+  } catch (e) {
+    num = 0
+  }
+  console.log(`Fetching pics with num: ${num}`)
+
+  const resp = await axios.get(`/Photo/list?category=cos&type=new&page_num=${num}&page_size=1`)
+  const { data } = resp.data
+  const items = data.items
+
+  const media = items[0].item.pictures.map(pic => {
+    return {
+      media: { url: pic.img_src },
+      caption: items[0].user.name + ' - ' + items[0].item.title,
+      type: 'photo'
+    }
+  })
+
+  ctx.replyWithMediaGroup(media)
+})
+
+bot.catch(err => {
+  console.log(err)
+})
 
 bot.startPolling()
