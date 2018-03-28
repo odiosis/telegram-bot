837229dc1800c9351d4c2bcc8ce285c0d549455b
jikkai <sonne@asaki.me>
2018-03-28 21:27:04 +0800

    [fix]replyWithMarkdown => reply

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 ## Usage
 
-* /pics [number]
+* /help: 获得帮助
+* /cos [n]: 获取 b 站相簿 cosplay 区第 n 张新相片集
+* /sifu [n]: 获取 b 站相簿私服区第 n 张新相片集
+* /illust [n]: 获取 b 站相簿插画区第 n 张新相片集
+* /comic [n]: 获取 b 站相簿漫画区第 n 张新相片集
+* /draw [n]: 获取 b 站相簿其他区第 n 张新相片集
 
 ## Development
 
--- features/help.js
+++ features/help.js
@@ -1,11 +1,13 @@
 const help = bot => {
-  bot.command('/help', ctx => ctx.replyWithMarkdown(`
-    * **/cos [n]**: 获取 b 站相簿 cosplay 区第 n 张新相片集
-    * **/sifu [n]**: 获取 b 站相簿私服区第 n 张新相片集
-    * **/illust [n]**: 获取 b 站相簿插画区第 n 张新相片集
-    * **/comic [n]**: 获取 b 站相簿漫画区第 n 张新相片集
-    * **/draw [n]**: 获取 b 站相簿其他区第 n 张新相片集
-  `))
+  bot.command('/help', ctx => {
+    ctx.reply(`
+      /cos [n]: 获取 b 站相簿 cosplay 区第 n 张新相片集
+      /sifu [n]: 获取 b 站相簿私服区第 n 张新相片集
+      /illust [n]: 获取 b 站相簿插画区第 n 张新相片集
+      /comic [n]: 获取 b 站相簿漫画区第 n 张新相片集
+      /draw [n]: 获取 b 站相簿其他区第 n 张新相片集
+    `)
+  })
 }
 
 module.exports = help
