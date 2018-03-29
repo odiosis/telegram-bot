8d36e0e70c665fedc7d7afd8e206c7dfd4e9202f
Jikkai Xiao <sonne@asaki.me>
2018-03-29 11:38:42 +0800

    [feature] add log

--- features/help.js
+++ features/help.js
@@ -1,12 +1,17 @@
+const helper = `
+/helper: 获取帮助
+/cos [n]: 获取 b 站相簿 cosplay 区第 n 张新相片集
+/sifu [n]: 获取 b 站相簿私服区第 n 张新相片集
+/illust [n]: 获取 b 站相簿插画区第 n 张新相片集
+/comic [n]: 获取 b 站相簿漫画区第 n 张新相片集
+/draw [n]: 获取 b 站相簿其他区第 n 张新相片集
+`
+
 const help = bot => {
-  bot.command('/help', ctx => {
-    ctx.reply(`
-      /cos [n]: 获取 b 站相簿 cosplay 区第 n 张新相片集
-      /sifu [n]: 获取 b 站相簿私服区第 n 张新相片集
-      /illust [n]: 获取 b 站相簿插画区第 n 张新相片集
-      /comic [n]: 获取 b 站相簿漫画区第 n 张新相片集
-      /draw [n]: 获取 b 站相簿其他区第 n 张新相片集
-    `)
+  bot.command('/help', async ctx => {
+    logger.info(`Fetching: Help`)
+    const replyer = await ctx.reply(helper)
+    logger.success(`Reply: help list sended`)
   })
 }
 
